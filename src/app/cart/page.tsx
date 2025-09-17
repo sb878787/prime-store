eb58369fbf51a39306bc83744e39911fe2289a7c
sb878787 <m.sb87.developer@gmail.com>
2025-09-17 13:15:49 +0330

    feat: add CartItemList component and dynamic cart summary
    - Added new `CartItemList` component to display cart items and summary.
    - Fetched product data from API and calculated total cart price dynamically.
    - Added `formatNumberWithCommas` utility for formatted price display.
    - Enhanced cart page with total price, discount, final price, and discount code

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -8,23 +8,6 @@ const Cart = () => {
       <h1 className="my-4">Cart</h1>
 
       <CartItemList />
-
-      <div className="border shadow-md p-4">
-        <h3 className="text-xl font-bold">
-          Total price: <span>$300</span>
-        </h3>
-        <h3 className="text-xl font-bold">
-          Discount: <span>$120</span>
-        </h3>
-        <h3 className="text-xl font-bold">
-          Final price: <span>$120</span>
-        </h3>
-
-        <div className="mt-3">
-          <input className="border px-4 py-2" placeholder="Discount code..." type="text" />
-          <button className="px-4 py-2 rounded bg-sky-500 text-white ml-4">Apply</button>
-        </div>
-      </div>
     </Container>
   );
 };
--- src/components/CartItemList.tsx
+++ src/components/CartItemList.tsx
@@ -1,19 +1,63 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 // Components
 import CartItem from './CartItem';
 
 // Hooks
 import { useShoppingCartContext } from '@/Hooks/useShoppingCartContext';
 
+// Types
+import { IProductItemProps } from '@/types/ProductItemType';
+
+// Functions
+import { formatNumberWithCommas } from '@/utils/number';
+
 const CartItemList = () => {
   const { cartItems } = useShoppingCartContext();
+  const [data, setData] = useState<IProductItemProps[]>([]);
+
+  useEffect(() => {
+    axios('https://fakestoreapi.com/products').then((res) => {
+      const { data } = res;
+      setData(data);
+    });
+  }, []);
 
   return (
     <div>
       {cartItems.map((item) => (
         <CartItem key={item.id} {...item} />
       ))}
+
+      <div className="border shadow-md p-4">
+        <h3 className="text-xl font-bold">
+          Total price:{' '}
+          <span>
+            $
+            {formatNumberWithCommas(
+              cartItems.reduce((total, item) => {
+                const selectedProduct = data.find((product) => product.id == item.id);
+
+                return total + (selectedProduct?.price || 0) * item.quantity;
+              }, 0),
+            )}
+          </span>
+        </h3>
+        <h3 className="text-xl font-bold">
+          Discount: <span>$120</span>
+        </h3>
+        <h3 className="text-xl font-bold">
+          Final price: <span>$120</span>
+        </h3>
+
+        <div className="mt-3">
+          <input className="border px-4 py-2" placeholder="Discount code..." type="text" />
+          <button className="px-4 py-2 rounded bg-sky-500 text-white ml-4">Apply</button>
+        </div>
+      </div>
     </div>
   );
 };
